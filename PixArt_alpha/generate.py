d0a5d904c5fcf442471b3ff7c1033c4a4e5adf10
Haozhe <53887227+HaozheLiu-ST@users.noreply.github.com>
2024-04-01 11:02:24 +0300

    Update generate.py

--- PixArt_alpha/generate.py
+++ PixArt_alpha/generate.py
@@ -2,7 +2,7 @@ import torch
 from pipeline_pixart_alpha import PixArtAlphaPipeline
 from customize_attention import register_custom_forward
 # You can replace the checkpoint id with "PixArt-alpha/PixArt-XL-2-512x512" too.
-gate_step = 10
+gate_step = 8
 inference_num_per_image = 25
 pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
 register_custom_forward(pipe.transformer, 'Attention',gate_step=gate_step,inference_num_per_image =inference_num_per_image)
