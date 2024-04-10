6376365d39071cae4806f618349d05126567acde
Wentian <94900022+WentianZhang-ML@users.noreply.github.com>
2024-04-10 18:48:56 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="tgate",
+    version="v0.1.0",
+    author=["Wentian Zhang", "Haozhe Liu", "Jinheng Xie"],
+    author_email=["zhangwentianml@gmail.com","haozhe.liu@kaust.edu.sa"],
+    description="T-GATE: Cross-Attention Makes Inference Cumbersome in Text-to-Image Diffusion Models.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HaozheLiu-ST/T-GATE/tree/release-v.0.1.0/",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages('src'),
+    include_package_data=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "torch>=2.0",
+        "diffusers>=0.27.2",
+        "transformers>=4.37.2",
+        "DeepCache",
+    ],
+    python_requires='>=3.8.0',
+)
