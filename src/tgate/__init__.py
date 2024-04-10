3e7ddb67357d8e032311c3e9563914383ce55801
Wentian <94900022+WentianZhang-ML@users.noreply.github.com>
2024-04-10 18:51:58 +0800

    Create __init__.py

---
+++ src/tgate/__init__.py
@@ -0,0 +1,5 @@
+from .PixArt_Alpha import TgatePixArtLoader
+from .SDXL import TgateSDXLLoader
+from .SD import TgateSDLoader
+from .SDXL_DeepCache import TgateSDXLDeepCacheLoader
+from .SD_DeepCache import TgateSDDeepCacheLoader
