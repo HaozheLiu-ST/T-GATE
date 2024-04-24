c8b1425f496351096ef92ae9a1b0718f5fbd5f25
Wentian <94900022+WentianZhang-ML@users.noreply.github.com>
2024-04-24 16:36:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import argparse
 import os
 
 import torch
-from LCM.customize_attention import register_custom_forward as lcm_register_custom_forward
-from PixArt_alpha.customize_attention import register_custom_forward as cfg_register_custom_forward
-from PixArt_alpha.pipeline_pixart_alpha import PixArtAlphaPipeline
-from SDXL.pipeline_stable_diffusion_xl import StableDiffusionXLPipeline
-from SD_2_1.stable_diffusion import StableDiffusionPipeline
-from diffusers import DPMSolverMultistepScheduler
-from diffusers import UNet2DConditionModel, LCMScheduler
 
+from tgate import TgateSDXLLoader,TgatePixArtLoader,TgateSDLoader,TgateSDDeepCacheLoader,TgateSDXLDeepCacheLoader
+from diffusers import PixArtAlphaPipeline,StableDiffusionXLPipeline,StableDiffusionPipeline
+from diffusers import UNet2DConditionModel, LCMScheduler
+from diffusers import DPMSolverMultistepScheduler
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple example of TGATE.")
@@ -38,13 +35,19 @@ def parse_args():
         default=10,
         help="When re-using the cross-attention",
     )
-
     parser.add_argument(
         "--inference_step",
         type=int,
         default=25,
         help="total inference steps",
     )
+    parser.add_argument(
+        '--deepcache', 
+        action='store_true', 
+        default=False, 
+        help='do deep cache',
+    )
+    
     args = parser.parse_args()
     return args
 
@@ -58,46 +61,54 @@ if __name__ == '__main__':
             repo_id = "runwayml/stable-diffusion-v1-5"
         elif args.model == 'sd_2.1':
             repo_id = "stabilityai/stable-diffusion-2-1"
-        pipe = StableDiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, revision="fp16")
 
+        pipe = StableDiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, variant="fp16")
         pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-        cfg_register_custom_forward(pipe.unet, 'Attention', gate_step=args.gate_step,
-                                    inference_num_per_image=args.inference_step)
+        if args.deepcache:
+            pipe = TgateSDDeepCacheLoader(pipe,cache_interval=3,cache_branch_id=0)
+        else:
+            pipe = TgateSDLoader(pipe)
         pipe = pipe.to("cuda")
+
         image = pipe.tgate(args.prompt,
-                         num_inference_steps=args.inference_step,
-                         guidance_scale=7.5,
-                         gate_step=args.gate_step,
-                         ).images[0]
+                        num_inference_steps=args.inference_step,
+                        guidance_scale=7.5,
+                        gate_step=args.gate_step,
+                        ).images[0]
+
+
     elif args.model == 'sd_xl':
         pipeline_text2image = StableDiffusionXLPipeline.from_pretrained(
             "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
         )
+
+        if args.deepcache:
+            pipeline_text2image = TgateSDXLDeepCacheLoader(pipeline_text2image,cache_interval=3,cache_branch_id=0)
+        else:
+            pipeline_text2image = TgateSDXLLoader(pipeline_text2image)
         pipeline_text2image.scheduler = DPMSolverMultistepScheduler.from_config(pipeline_text2image.scheduler.config)
-        cfg_register_custom_forward(pipeline_text2image.unet, 'Attention', gate_step=args.gate_step,
-                                    inference_num_per_image=args.inference_step)
         pipeline_text2image = pipeline_text2image.to("cuda")
+
         image = pipeline_text2image.tgate(prompt=args.prompt,
                                           gate_step=args.gate_step,
                                           num_inference_steps=args.inference_step).images[0]
     elif args.model == 'pixart':
         pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
-        cfg_register_custom_forward(pipe.transformer, 'Attention', gate_step=args.gate_step,
-                                    inference_num_per_image=args.inference_step)
-        pipe = pipe.to("cuda")
+        pipe = TgatePixArtLoader(pipe).to("cuda")
         image = pipe.tgate(args.prompt,
                            gate_step=args.gate_step,
                            num_inference_steps=args.inference_step).images[0]
 
+
     elif args.model == 'lcm_pixart':
         pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-LCM-XL-2-1024-MS", torch_dtype=torch.float16)
-        lcm_register_custom_forward(pipe.transformer, 'Attention', gate_step=args.gate_step,
-                                    inference_num_per_image=args.inference_step)
-        pipe = pipe.to("cuda")
-        image = pipe(args.prompt,
-                     gate_step=args.gate_step,
-                     num_inference_steps=args.inference_step,
-                     guidance_scale=0).images[0]
+        pipe = TgatePixArtLoader(pipe,lcm=True).to("cuda")
+        image = pipe.tgate(
+            args.prompt,
+            gate_step=args.gate_step,
+            num_inference_steps=args.inference_step,
+            guidance_scale=0.).images[0]
+
 
     elif args.model == 'lcm_sdxl':
         unet = UNet2DConditionModel.from_pretrained(
@@ -107,14 +118,16 @@ if __name__ == '__main__':
         )
         pipe = StableDiffusionXLPipeline.from_pretrained(
             "stabilityai/stable-diffusion-xl-base-1.0", unet=unet, torch_dtype=torch.float16, variant="fp16",
-        ).to("cuda")
+        )
         pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
-        lcm_register_custom_forward(pipe.unet, 'Attention', gate_step=args.gate_step,
-                                    inference_num_per_image=args.inference_step)
+        pipe = TgateSDXLLoader(pipe,lcm=True).to("cuda")
 
-        image = pipe(
-            prompt=args.prompt, num_inference_steps=args.inference_step
+        image = pipe.tgate(
+            prompt=args.prompt, 
+            gate_step=args.gate_step,
+            num_inference_steps=args.inference_step,
         ).images[0]
+
     else:
         raise Exception('Please sepcify the model name!')
     image.save(saved_path)
